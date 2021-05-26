416421e2b348dbafb03e95b03c31becea57a5b96
Arsen Niyonzima <arse0012@edu.easj.dk>
2021-05-26 20:14:48 +0200

    cleaning up

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import CreateUser from "./components/createUser";
-import CreateExercise from "./components/create-exercise.component";
-import EditExercise from "./components/edit-exercise.component";
-import ExercisesList from "./components/exercises-list.component";
+import CreateUser from "./components/create-user";
+import CreateExercise from "./components/create-exercise";
+import EditExercise from "./components/edit-exercise";
+import ExercisesList from "./components/exercises-list";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <Route path="/" component={ExercisesList} />
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
         <Route path="/user" component={CreateUser} />
-        <Route path="/exercise" component={CreateExercise} />
-        <Route path="/edit" component={EditExercise} />
-        
       </div>
     </Router>
   );
--- src/components/create-exercise.js
+++ src/components/create-exercise.js
@@ -130,7 +130,7 @@ export default class CreateExercise extends Component {
                     </div>
 
                     <div className="form-group">
-                        <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+                        <input type="submit" value="Log Exercise" className="btn btn-primary" />
                     </div>
                 </form>
             </div>
