768d18ad23f1c3a2109f2d5ae86b1f0c9f405895
Arsen Niyonzima <arse0012@edu.easj.dk>
2021-05-26 12:27:16 +0200

    mongoDB-backend

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from "./components/navbar.component"
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
+import CreateUser from "./components/createUser";
 import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
+import EditExercise from "./components/edit-exercise.component";
+import ExercisesList from "./components/exercises-list.component";
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <Navbar />
-        <br />
-        <Route path="/" exact component={ExercisesList} />
-        <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={CreateExercise} />
+        <Route path="/" component={ExercisesList} />
         <Route path="/user" component={CreateUser} />
+        <Route path="/exercise" component={CreateExercise} />
+        <Route path="/edit" component={EditExercise} />
+        
       </div>
     </Router>
   );
